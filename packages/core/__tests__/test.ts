8eab4f9e6bb51a102a5abdfbd00c46476a3db748
imeepos <1037483576@qq.com>
2019-08-26 19:56:22 +0800

    fix: 🐛 options or conditions

---
+++ packages/core/__tests__/test.ts
@@ -0,0 +1,9 @@
+// import { createResolvers } from "../lib";
+
+// const metadata = require("./assets/fastity/magnus.metadata.json");
+// const magnus = require("./assets/fastity/magnus.server.json");
+// const entities = require("./assets/fastity/magnus.entity.json");
+
+// createResolvers(metadata, entities, {}, (name: string) => {
+//     return {}
+// });
