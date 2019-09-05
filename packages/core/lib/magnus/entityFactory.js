c2cd9b55231242a06aaa1ea3671989e62b291f01
meepo <1037483576@qq.com>
2019-09-05 14:45:18 +0800

    fix: 🐛 fix members map

--- packages/core/lib/magnus/entityFactory.js
+++ packages/core/lib/magnus/entityFactory.js
@@ -6,34 +6,39 @@ class EntityFactory {
     }
     create(instance, name) {
         const members = this.options.entities[name];
-        const methods = members.map((member) => {
-            if (member.decorators.includes('ResolveProperty')) {
-                return member.name;
-            }
-        }).filter((it) => !!it);
-        const createSet = this.options.createSet;
-        const options = this.options;
-        return new Proxy(instance, {
-            get(target, p, receiver) {
-                if (methods.includes(p)) {
-                    return (variables, context, info) => {
-                        const set = createSet({
-                            entities: options.entities,
-                            decorators: options.decorators,
-                            enums: options.enums,
-                            info,
-                            context,
-                            variables
-                        });
-                        const args = set.getArguments();
-                        return target[p].bind(target)(...args);
-                    };
+        if (members) {
+            const methods = members
+                .map(member => {
+                if (member.decorators.includes("ResolveProperty")) {
+                    return member.name;
                 }
-                else {
-                    return target[p];
+            })
+                .filter(it => !!it);
+            const createSet = this.options.createSet;
+            const options = this.options;
+            return new Proxy(instance, {
+                get(target, p, receiver) {
+                    if (methods.includes(p)) {
+                        return (variables, context, info) => {
+                            const set = createSet({
+                                entities: options.entities,
+                                decorators: options.decorators,
+                                enums: options.enums,
+                                info,
+                                context,
+                                variables
+                            });
+                            const args = set.getArguments();
+                            return target[p].bind(target)(...args);
+                        };
+                    }
+                    else {
+                        return target[p];
+                    }
                 }
-            }
-        });
+            });
+        }
+        return instance;
     }
 }
 exports.EntityFactory = EntityFactory;
--- packages/core/lib/magnus/entityFactory.ts
+++ packages/core/lib/magnus/entityFactory.ts
@@ -1,44 +1,49 @@
 interface Member {
-    name: string;
-    decorators: string[];
-    parameters: any[];
+  name: string;
+  decorators: string[];
+  parameters: any[];
 }
 interface FactoryOptions {
-    enums?: any;
-    entities: any;
-    decorators: any;
-    createSet: any;
+  enums?: any;
+  entities: any;
+  decorators: any;
+  createSet: any;
 }
 export class EntityFactory {
-    constructor(private options: FactoryOptions) { }
-    create<T extends object>(instance: T, name: string) {
-        const members = this.options.entities[name];
-        const methods = members.map((member) => {
-            if (member.decorators.includes('ResolveProperty')) {
-                return member.name;
-            }
-        }).filter((it) => !!it);
-        const createSet = this.options.createSet;
-        const options = this.options;
-        return new Proxy(instance, {
-            get(target, p, receiver) {
-                if (methods.includes(p)) {
-                    return (variables, context, info) => {
-                        const set = createSet({
-                            entities: options.entities,
-                            decorators: options.decorators,
-                            enums: options.enums,
-                            info,
-                            context,
-                            variables
-                        });
-                        const args = set.getArguments();
-                        return target[p].bind(target)(...args);
-                    };
-                } else {
-                    return target[p]
-                }
-            }
-        });
+  constructor(private options: FactoryOptions) {}
+  create<T extends object>(instance: T, name: string) {
+    const members = this.options.entities[name];
+    if (members) {
+      const methods = members
+        .map(member => {
+          if (member.decorators.includes("ResolveProperty")) {
+            return member.name;
+          }
+        })
+        .filter(it => !!it);
+      const createSet = this.options.createSet;
+      const options = this.options;
+      return new Proxy(instance, {
+        get(target, p, receiver) {
+          if (methods.includes(p)) {
+            return (variables, context, info) => {
+              const set = createSet({
+                entities: options.entities,
+                decorators: options.decorators,
+                enums: options.enums,
+                info,
+                context,
+                variables
+              });
+              const args = set.getArguments();
+              return target[p].bind(target)(...args);
+            };
+          } else {
+            return target[p];
+          }
+        }
+      });
     }
+    return instance;
+  }
 }
